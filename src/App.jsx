a7d3038b7cb8662189edf329ae17be54c8f03bd4
Ervand Sargsyan <sargsyanervand2010.ait@gmail.com>
2024-02-08 22:53:32 +0400

    create layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
 
 export default function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path='/' element=''>
+				<Route path='/' element={<MainLayout />}>
 					<Route path='/' element='' />
 				</Route>
 			</Routes>
