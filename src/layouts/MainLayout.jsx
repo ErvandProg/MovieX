48d01657c7ea4f3b166f4c06e2011a44496f0e95
Ervand Sargsyan <sargsyanervand2010.ait@gmail.com>
2024-02-10 08:51:48 +0400

    create footer

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from '../components/header/Header';
 import { Outlet } from 'react-router-dom';
-// import Footer from '../components/footer/Footer';
+import Footer from '../components/footer/Footer';
 
 export default function MainLayout() {
 	return (
 		<div className='w-[100%] h-[100%] flex flex-col justify-between'>
 			<Header />
 			<Outlet />
-			{/* <Footer /> */}
+			<Footer />
 		</div>
 	)
 }
